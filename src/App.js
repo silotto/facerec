f23bf7b4159f560c6083316b593fa583dba3586f
Joovay <Juvi925@gmail.com>
2018-01-16 20:07:33 -0500

    clean up some of the code syntax and whitespacing for readability, no actual functionality changes at the moment

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Clarifai from 'clarifai';
 
 const app = new Clarifai.App({
   apiKey: 'd77276943a2643619c9b7f3b4b9ad837'
- });
+});
 
 
 const particlesOptions = {
@@ -46,15 +46,17 @@ class App extends Component {
     }
   }
 
-loadUser = (data) => {
-  this.setState({user: {
-    id: data.id,
-    name: data.name,
-    email: data.email,
-    entries: data.entries,
-    joined: data.joined
-  }})
-}
+  loadUser = (data) => {
+    this.setState({user: 
+      {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+      }
+    })
+  }
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -78,9 +80,16 @@ loadUser = (data) => {
   }
 
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input})
-      app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    this.setState({imageUrl: this.state.input});
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(response => { if (response) 
+        {fetch('http://localhost:3000/image', 
+        {method: 'put',headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({id: this.state.user.id})})
+      .then(response => response.json())
+      .then(count => {this.setState(Object.assign(this.state.user, 
+        { entries: count }))})}this.displayFaceBox(this.calculateFaceLocation(response))})
       .catch(err => console.log(err));
   }
 
@@ -90,32 +99,44 @@ loadUser = (data) => {
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
-    this.setState({route: route});
+      this.setState({route: route});
   }
 
   render() {
     const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
-      <Particles className='particles'
-              params={particlesOptions}/>
-        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
-        { route === 'home' 
-        ? <div>
-            <Logo />
-            <Rank />        
-            <ImageLinkForm 
-              onInputChange={this.onInputChange} 
-              onButtonSubmit={this.onButtonSubmit}/>
-            {<FaceRecognition box={box} imageUrl={imageUrl} />}
-          </div>
-          
-        :(
-          route === 'signin' 
-          ? <Signin onRouteChange={this.onRouteChange} />
-          : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+        <Particles 
+          className='particles'
+          params={particlesOptions}
+        />
+        
+        <Navigation 
+          isSignedIn={isSignedIn} 
+          onRouteChange={this.onRouteChange} 
+        />
 
-        ) 
+        { route === 'home' 
+          ? <div>
+              <Logo />
+              <Rank 
+                name={this.state.user.name} 
+                entries={this.state.user.entries} 
+              />        
+              <ImageLinkForm 
+                onInputChange={this.onInputChange} 
+                onButtonSubmit={this.onButtonSubmit}
+              />
+              <FaceRecognition 
+                box={box} 
+                imageUrl={imageUrl} 
+              />
+            </div>
+          : ( 
+              route === 'signin' 
+              ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+            ) 
         }
       </div>
     );
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,13 +5,15 @@ import brain from './brain.png';
 
 
 const Logo = () => {
-    return (
-        <div className='ma4 mt0'>
-            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
-            <div className="Tilt-inner pa3"> <img style={{paddingTop: '5px'}} src={brain} alt="brain logo"/> </div>
-            </Tilt>
+  return (
+    <div className='ma4 mt0'>
+      <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }}>
+        <div className="Tilt-inner pa3">
+         <img style={{paddingTop: '5px'}} src={brain} alt="brain logo"/>
         </div>
-    );
+      </Tilt>
+    </div>
+  );
 }
 
 export default Logo;
