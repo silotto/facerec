47d81510fc0acf92c020fef30a1e168140a92628
Joovay <Juvi925@gmail.com>
2018-01-23 20:48:56 -0500

    moved the clarifai api to the server

--- src/App.js
+++ src/App.js
@@ -8,11 +8,9 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
 import './App.css';
-import Clarifai from 'clarifai';
 
-const app = new Clarifai.App({
-  apiKey: 'd77276943a2643619c9b7f3b4b9ad837'
-});
+
+
 
 
 const particlesOptions = {
@@ -81,10 +79,14 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL, 
-        this.state.input)
+    fetch('http://localhost:3000/imageurl', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          input: this.state.input
+        })
+      })
+      .then(response => response.json())
       .then(response => {
         if (response) {
           fetch('http://localhost:3000/image', {
